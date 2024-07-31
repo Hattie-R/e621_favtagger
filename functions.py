7b80b070aa4e7a10e25d949b81ce0dbd35a7b4ee
Hattie-R <baurzanzapparov@gmail.com>
2024-07-31 08:21:11 +0800

    OOP application (separation of functions from the main executable file) Tried to fix the removing of original images

---
+++ functions.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def downl_url_and_artist(post_id):
+    api_url = f"https://e621.net/posts/{post_id}.json"
+    headers = {'user-agent': 'hydrusBatchSauce/corposim'}
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        image_url = json_data["post"]["file"]["url"]
+        artists = json_data["post"]["tags"]["artist"]
+        deleted = json_data["post"]["flags"]["deleted"] #checks if the post was deleted or not
+        if deleted == 'true':
+            print('Post has been deleted, check it yourself')
+        return image_url, artists, deleted
+    else:
+        print(f"Failed to get image URL. Status code: {response.status_code}")
+        return None
+
+def download_image(url, id, artist):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open("{}-{}.{}".format('_'.join(artist), id, url.split('.')[-1]), "wb") as file:
+            file.write(response.content)
+        print("Image downloaded successfully.\n")
+    else:
+        print(f"Failed to download image. Status code: {response.status_code}")
+
+def favorite_post(id,USERNAME, API_KEY):
+    headers = {'user-agent': 'hydrusBatchSauce/corposim'}
+    auth = requests.auth.HTTPBasicAuth(USERNAME, API_KEY)
+    requests.post(url="https://e621.net/favorites.json", auth=auth, headers=headers, json={ "post_id": id })
+
+if __name__ == "__main__":
+    pass
--- main.py
+++ main.py
@@ -2,41 +2,7 @@ import requests
 import time
 import os
 import shutil
-
-
-def downl_url_and_artist(post_id):
-    api_url = f"https://e621.net/posts/{post_id}.json"
-    headers = {'user-agent': 'hydrusBatchSauce/corposim'}
-    response = requests.get(api_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        image_url = json_data["post"]["file"]["url"]
-        artists = json_data["post"]["tags"]["artist"]
-        deleted = json_data["post"]["flags"]["deleted"] #checks if the post was deleted or not
-        if deleted == 'true':
-            print('Post has been deleted, check it yourself')
-        return image_url, artists, deleted
-    else:
-        print(f"Failed to get image URL. Status code: {response.status_code}")
-        return None
-
-def download_image(url, id, artist):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open("{}-{}.{}".format('_'.join(artist), id, url.split('.')[-1]), "wb") as file:
-            file.write(response.content)
-        print("Image downloaded successfully.\n")
-    else:
-        print(f"Failed to download image. Status code: {response.status_code}")
-
-def favorite_post(id):
-    headers = {'user-agent': 'hydrusBatchSauce/corposim'}
-    auth = requests.auth.HTTPBasicAuth(USERNAME, API_KEY)
-    requests.post(url="https://e621.net/favorites.json", auth=auth, headers=headers, json={ "post_id": id })
-
-
-
+from functions import *
 
                                         # Taking basic info
 print("Hello!, may I help you with you yiff collection?\nBut first, I'd like to know where is your collection.\nProvide with smth like C:\\Users\\Hattie Ragales\\Pictures\\yiff")
@@ -74,9 +40,10 @@ for path in list1:
         idp = str(r.json()[0]['post_id'])
         print(str(counter) + '. Success for: ' + path + ' : ' + urls)
      
-        favorite_post(idp)
+        favorite_post(idp, USERNAME, API_KEY)
         Found.append(path)
         url1 = downl_url_and_artist(idp)
+        os.remove(path)
         download_image(url1[0], idp, url1[1])
 
     except:                              #It didn't find
@@ -92,9 +59,10 @@ for path in list1:
 
 # Sortation of files found and not found on e621
 os.mkdir('no_e621')
+os.mkdir('e621')
 print('Found:' + '\n' + str(Found))
 for i in Found:
-    os.remove(i)
+    shutil.move(i, dir+'\\e621')
 print('Not Found:' + '\n' + str(notFound))
 
 for i in notFound:
